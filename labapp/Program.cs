52a1a681fd69ec011f7a5a7ac2b7efc059638876
|KamilC| <kamil2523@gmail.com>
2019-11-24 10:22:32 +0100

    Poprawka

--- labapp/Program.cs
+++ labapp/Program.cs
@@ -12,24 +12,15 @@ namespace labapp
         {
             while (true)
             {
-                int menuOption = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("1. Dodaj liczby");
-
-                Console.WriteLine("4. Dziel liczby");
-                Console.WriteLine("5. Wyjscie");
                 Console.WriteLine("2. Odejmij liczby");
                 Console.WriteLine("3. * liczby liczb");
-
-                int a = Convert.ToInt32(Console.ReadLine());
-
-
-<<<<<<< HEAD
-=======
+                Console.WriteLine("4. Dziel liczby");
+                Console.WriteLine("5. Wyjscie");
                 int menuOption = Convert.ToInt32(Console.ReadLine());
-
                 if (menuOption == 5)
                     break;
->>>>>>> dcc2935... Exit option is now implemented
+                int a = Convert.ToInt32(Console.ReadLine());
             }
 
         }
